5dea83616d8775b3e8089e16e093b159e02d5d5e
Đoàn Thị Diễm <diemdt@dc1.gemvietnam.com>
2025-08-04 15:06:43 +0700

    photo completed

---
+++ src/main/java/com/diemdt/restaurant/services/PhotoService.java
@@ -0,0 +1,12 @@
+package com.diemdt.restaurant.services;
+
+import com.diemdt.restaurant.domain.entities.Photo;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+public interface PhotoService {
+    Photo uploadPhoto(MultipartFile file);
+    Optional<Resource> getPhotoAsResource(String id);
+}
